6fe4dbfc130b783be9a8cc64eb892824948e8afa
sanjayy <sanjay2808a@gmail.com>
2026-01-15 21:45:04 +0530

    \

---
+++ Missing_num.cpp
@@ -0,0 +1,35 @@
+/*
+    Find the missing number in the Array
+    SUM METHOD:
+      Actual(Sumation)
+      Expected=n(n+1)/2
+      Missing=Expected-Actual
+    
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<int> arr(100);
+    int n,missing;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    cout<<"Enter the array elements from 0 to n: ";
+    int exp=0,act=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        act+=arr[i];
+    }
+    exp= (n*(n+1))/2;
+    missing = exp-act;
+    cout<<"Missing number: "<<missing;
+
+}
