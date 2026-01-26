a3c6d171eda5e33f1995454b0510e88d63434804
Sanjay <sanjay2808a@gmail.com>
2026-01-26 22:51:11 +0530

    Non Repeating String

---
+++ Strings/Non_rep-str.cpp
@@ -0,0 +1,34 @@
+// To return first non repeating string
+//time complexity: O(n²)
+//brute-force
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+    vector<string> arr = {"apple", "banana", "grapes", "orange", "apple", "orange", "banana"};
+    string res = "NONE";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        bool dup = true;
+
+        for (int j = 0; j < arr.size(); j++)
+        {
+            if (i == j)
+                continue;
+
+            if (arr[i] == arr[j])
+            {
+                dup = false;
+                break;
+            }
+        }
+        if (dup)
+        {
+            res = arr[i];
+            break;
+        }
+    }
+    cout << res;
+}
